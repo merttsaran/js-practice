032fb5cf5ded1bc6620e863d180b97367af1114f
merttsaran <merttsaran@gmail.com>
2026-03-15 23:31:04 +0300

    style: add CSS styling to todo list

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -18,17 +18,15 @@ function renderTodoList() {
     //const dueDate = todoObject.dueDate;
     const { name, dueDate } = todoObject;
     const html = `
-      <p>
-        ${name} ${dueDate} 
-        <button onclick="
-          todoList.splice(${i}, 1);
-          renderTodoList();
-        ">Delete</button>
-      </p>
+      <div>${name}</div>
+      <div>${dueDate}</div>
+      <button onclick="
+        todoList.splice(${i}, 1);
+        renderTodoList();
+      " class="delete-todo-button">Delete</button>
     `;
     todoListHTML += html;
   }
-  console.log(todoListHTML);
 
   document.querySelector('.js-todo-list')
     .innerHTML = todoListHTML;
@@ -47,7 +45,6 @@ function addTodo() {
     name,
     dueDate
   });
-  console.log(todoList);
 
   inputElement.value = '';
 
