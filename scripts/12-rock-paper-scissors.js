aa3630185651dbc5110bd8781c53067ccb543f5d
merttsaran <merttsaran@gmail.com>
2026-03-20 01:32:23 +0300

    feat: add keyboard controls (r, p, s) using event.key

--- scripts/12-rock-paper-scissors.js
+++ scripts/12-rock-paper-scissors.js
@@ -47,6 +47,15 @@
           playGame('scissors');
         });  
 
+      document.body.addEventListener('keydown', (event) => {
+        if (event.key === 'r') {
+          playGame('rock');
+        } else if (event.key === 'p') {
+          playGame('paper');
+        } else if (event.key === 's') {
+          playGame('scissors');
+        }
+      });
         
       
       function playGame(playerMove) {
