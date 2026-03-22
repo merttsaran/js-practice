73cf4f6423dea5c495c50a5fe377bb24ec698128
merttsaran <merttsaran@gmail.com>
2026-03-23 02:41:11 +0300

    refactor: replace onclick with addEventListener for reset score button

--- scripts/12r-rock-paper-scissors.js
+++ scripts/12r-rock-paper-scissors.js
@@ -144,3 +144,16 @@
 
         return computerMove;
       }
+
+      function resetScore() {
+        score.wins = 0;
+        score.losses = 0;
+        score.ties = 0;
+        localStorage.removeItem('score');
+        updateScoreElement();
+      }
+
+      document.querySelector('.js-reset-score-button')
+        .addEventListener('click', () => {
+          resetScore();
+        });
